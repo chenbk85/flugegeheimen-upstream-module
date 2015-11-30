2eaefb25fddbfeee004538258cb51e700322d3a5
Alexandr Chernakov <alexandr.chernakov@gmail.com>
2015-11-30 15:37:55 +0300

    Somewhat close to finishing upstream module

--- flug_upstream_module.c
+++ flug_upstream_module.c
@@ -12,15 +12,24 @@
 #define FLUG_UPSTREAM_ADDR "127.0.0.1"
 #define FLUG_UPSTREAM_PORT "2345"
 #define FLUG_UPSTREAM_LOG "nginx/logs/flug_upstream.log"
+#define FLUG_UPSTREAM_MIME "application/json"
+
+#define FLUG_DEBUG_LOG 1
 
 static time_t flug_log_timestamp (FILE * log) {
 	time_t t;
+	char data[100], *ptr;
 	time(&t);
-	fprintf(log, "%s: ", ctime(&t));
+	strcpy(data, ctime(&t));
+	for(ptr = data; ((*ptr = (*ptr == '\n')?' ':*ptr),(*ptr)); ptr++);
+	fprintf(log, "%s: ", data);
 	return t;
 }
 
 static void flug_log_cstr (const char * str) {
+	if (!FLUG_UPSTREAM_LOG) {
+		return;
+	}
 	FILE * log = fopen(FLUG_UPSTREAM_LOG, "a");
 	if (!log) {
 		return;
@@ -33,7 +42,10 @@ static void flug_log_cstr (const char * str) {
 
 }
 
-/*static void flug_log_nstr (ngx_str_t str) {
+static void flug_log_nstr (ngx_str_t str) {
+	if (!FLUG_UPSTREAM_LOG) {
+		return;
+	}
 	FILE * log = fopen(FLUG_UPSTREAM_LOG, "a");
 	if (!log) {
 		return;
@@ -44,7 +56,24 @@ static void flug_log_cstr (const char * str) {
 	fprintf (log, "\n");
 
 	fclose(log);
-}*/
+}
+
+static void flug_log_numeric_parameter (const char * name, uint32_t number) {
+
+	if (!FLUG_UPSTREAM_LOG) {
+		return;
+	}
+	FILE * log = fopen(FLUG_UPSTREAM_LOG, "a");
+	if (!log) {
+		return;
+	}
+
+	flug_log_timestamp(log);
+	fprintf (log, "%s: %u\n",  name, number);
+
+	fclose(log);
+
+}
 
 typedef struct {
 	ngx_http_upstream_conf_t upstream;
@@ -54,8 +83,11 @@ typedef struct {
 
 
 //Module context struct
-typedef struct flug_module_context_s {
-} flug_module_context_t;
+typedef struct flug_request_context_s {
+	uint32_t sent;
+	uint32_t size;
+	ngx_int_t initDone: 1;
+} flug_request_context_t;
 
 
 void * flug_create_loc_conf (ngx_conf_t * cf);
@@ -129,7 +161,7 @@ static ngx_http_module_t * flug_get_context (ngx_http_request_t * r) {
 
 //Sockaddr/socklen structs resolving
 
-/*static ngx_int_t flug_resolve_upstream (ngx_http_upstream_resolved_t * resolved) {
+static ngx_int_t flug_resolve_upstream (ngx_http_upstream_resolved_t * resolved) {
 	int ret;
 	int sfd = -1;
 	bool connected = false;
@@ -139,7 +171,7 @@ static ngx_http_module_t * flug_get_context (ngx_http_request_t * r) {
 	flug_log_cstr ("Resolving address");
 
 	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_INET;    
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM; 
 
 	ret = getaddrinfo("127.0.0.1", "2345", &hints, &results);
@@ -167,7 +199,7 @@ static ngx_http_module_t * flug_get_context (ngx_http_request_t * r) {
 	} 
 
 	return NGX_OK;
-}*/
+}
 
 
 
@@ -177,17 +209,149 @@ static ngx_http_module_t * flug_get_context (ngx_http_request_t * r) {
 	return NULL;
 }*/
 
-static ngx_int_t flug_upstream_process_header(ngx_http_request_t * r) {
-	flug_log_cstr("flug_upstream_process_header");
-	return NGX_ERROR;
+
+
+static ngx_int_t flug_chain_to_str (ngx_http_request_t * r,
+								ngx_chain_t * chain,
+								ngx_str_t * str) {
+	ngx_chain_t * ptr;
+	ngx_int_t size = 0, counter = 0;
+	for (ptr = chain; ptr; ptr = ptr->next)
+		size += ptr->buf->last - ptr->buf->pos;
+
+	str->data = ngx_pcalloc(r->pool, size);
+	if (!str->data) {
+		return -1;
+	}
+
+	for (ptr = chain; ptr; ptr = ptr->next) {
+		memcpy(str->data + counter, ptr->buf->pos, ptr->buf->last - ptr->buf->pos);
+		counter += ptr->buf->last - ptr->buf->pos;
+	}
+	str->len = size;
+
+	return 0;
 }
+
+
+static ngx_int_t flug_upstream_process_header(ngx_http_request_t * r) {
+	ngx_http_upstream_t       *u;
+	uint32_t respSize, rawSize;
+
+	u = r->upstream;
+	flug_log_cstr("flug_upstream_process_header");
+	rawSize = u->buffer.last - u->buffer.pos;
+	flug_log_numeric_parameter("Size of data transmited by upstream", rawSize);
+	flug_log_numeric_parameter("Size of data by start/end pair", 
+			(uint32_t)(u->buffer.end - u->buffer.start));
+
+	flug_request_context_t * ctx = 
+			ngx_http_get_module_ctx(r, ngx_http_flug_upstream_module);
+
+	if (!ctx) {
+		flug_log_cstr("Failed to get context");
+		return NGX_ERROR;
+	}
+
+	ngx_memcpy((u_char*)&respSize, u->buffer.pos, sizeof(uint32_t));
+
+	r->headers_out.content_type.len = sizeof(FLUG_UPSTREAM_MIME) - 1;
+	r->headers_out.content_type.data = (u_char *) FLUG_UPSTREAM_MIME;
+	u->buffer.pos += sizeof(uint32_t);
+
+	u->state->status = NGX_HTTP_OK;
+	u->headers_in.status_n = NGX_HTTP_OK;
+
+	//u->length = respSize + sizeof(uint32_t);
+	u->headers_in.content_length_n = respSize;
+
+
+	return NGX_OK;
+
+}
+
 static void flug_upstream_finalize_request(ngx_http_request_t * r, ngx_int_t rc) {
 	flug_log_cstr("flug_upstream_finalize_request");
 }
+
+static flug_request_context_t * flug_upstream_new_request_ctx (ngx_http_request_t * r) {
+	flug_log_cstr("Creating new request context");
+
+	
+	flug_request_context_t * reqCtx = ngx_pcalloc(r->pool, sizeof (flug_request_context_t));
+	if (!reqCtx) {
+		return NULL;
+	}
+
+	reqCtx->size = 0;
+	reqCtx->sent = 0;
+	reqCtx->initDone = 0;
+
+	ngx_http_set_ctx(r, reqCtx, ngx_http_flug_upstream_module);
+	return reqCtx;
+}
+
 static ngx_int_t flug_upstream_create_request(ngx_http_request_t * r) {
 	//ngx_str_t dummyRequest = ngx_string("{\"subsystem\":\"asdfasd\"}");
+	ngx_int_t err;
+	ngx_str_t postData = {0, NULL};
+	ngx_buf_t * upstream_buf;
+	uint32_t upstream_len;
+	
 	flug_log_cstr ("flug_upstream_create_request");
+
+	flug_request_context_t * reqCtx = flug_upstream_new_request_ctx(r);
+	if (!reqCtx) {
+		flug_log_cstr("Failed to create rewuest context");
+		return NGX_ERROR;
+	}
+
+	err = flug_chain_to_str (r, r->request_body->bufs, &postData);
+	if (err) {
+		flug_log_cstr ("Failed to get POST body from chain");
+		return NGX_ERROR;
+	}
+
+
+	flug_log_nstr (postData);
+
+	//create request to flugegeheimen server out of postData
+	
+	r->upstream->request_bufs = ngx_alloc_chain_link (r->pool);
+	if (r->upstream->request_bufs == NULL) {
+		flug_log_cstr("Failed to allocate chain link to store upstream request");
+		return NGX_ERROR;
+	}
+
+	upstream_len = postData.len;
+	upstream_buf = ngx_create_temp_buf (r->pool, postData.len + sizeof (uint32_t));
+	ngx_memcpy(upstream_buf->pos + sizeof(uint32_t), postData.data, postData.len);
+	ngx_memcpy(upstream_buf->pos, (u_char*)&upstream_len, sizeof(uint32_t));
+	upstream_buf->last = upstream_buf->pos + upstream_len + sizeof(uint32_t);
+	
+
+	r->upstream->request_bufs->buf = upstream_buf;
+	r->upstream->request_bufs->next = NULL;
+
+	r->upstream->request_sent = 0;
+	r->upstream->header_sent = 0;
+
+	r->header_hash = 0;
+
 	return NGX_OK;
+}
+
+ngx_int_t flug_upstream_input_filter (void * data, ssize_t bytes) {
+	flug_log_cstr("flug_upstream_input_filter");
+	return NGX_OK;
+}
+
+void flug_upstream_abort_request (ngx_http_request_t * r) {
+	flug_log_cstr("ERROR! flug_upstream_abort_request");
+}
+
+ngx_int_t flug_upstream_reinit_request () {
+	return NGX_ERROR;
 }
 
 //Setup the connection details like as sockaddr/socklen structs
@@ -200,10 +364,6 @@ static ngx_int_t flug_setup_upstream (ngx_http_request_t * r, ngx_http_module_t 
 		return NGX_ERROR;
 	}
 
-	/*ngx_http_upstream_t * u = ngx_palloc(r->pool, sizeof(ngx_http_upstream_t));
-	if (!u) {
-		return NGX_ERROR;
-	}*/
 
 	flug_upstream_conf_t *mycf = (flug_upstream_conf_t*)
 			ngx_http_get_module_loc_conf(r,ngx_http_flug_upstream_module);
@@ -227,27 +387,11 @@ static ngx_int_t flug_setup_upstream (ngx_http_request_t * r, ngx_http_module_t 
 		return NGX_ERROR;
 	}
 
-	static struct sockaddr_in backendSockAddr;
-	struct hostent *pHost = gethostbyname((char*) "www.google.com");
 
-
-	if(pHost == NULL){
-		ngx_log_error(NGX_LOG_DEBUG,r->connection->log,0,"gethostbyname error:%s",strerror(errno));
+	if (flug_resolve_upstream(u->resolved) != NGX_OK) {
+		flug_log_cstr("Failed to resolve route to flugegeheimen server, bitch");
 		return NGX_ERROR;
 	}
-	//Access to the upstream server porflug_request_handlert
-	backendSockAddr.sin_family = AF_INET;
-	backendSockAddr.sin_port = htons((in_port_t)80);
-	char* pDmsIP = inet_ntoa(*(struct in_addr*) (pHost->h_addr_list[0]));
-	backendSockAddr.sin_addr.s_addr = inet_addr(pDmsIP);
-	//myctx->backendServer.data = (u_char * )pDmsIP;
-	//myctx->backendServer.len = strlen(pDmsIP);
-
-
-	//Set the address to resolved members
-	u->resolved->sockaddr = (struct sockaddr *)&backendSockAddr;
-	u->resolved->socklen = sizeof(struct sockaddr_in );
-	u->resolved->naddrs = 1;
 
 	u->peer.log = r->connection->log;
 	u->peer.log_error = NGX_ERROR_ERR;
@@ -255,6 +399,10 @@ static ngx_int_t flug_setup_upstream (ngx_http_request_t * r, ngx_http_module_t 
 	u->create_request = flug_upstream_create_request;
 	u->process_header = flug_upstream_process_header;
 	u->finalize_request = flug_upstream_finalize_request;
+	
+	//u->input_filter = flug_upstream_input_filter;
+	//u->abort_request = flug_upstream_abort_request;
+	//u->reinit_request = flug_upstream_reinit_request;
 
 	r->upstream = u;
 
@@ -263,7 +411,6 @@ static ngx_int_t flug_setup_upstream (ngx_http_request_t * r, ngx_http_module_t 
 
 //Main request handler
 static ngx_int_t flug_request_handler (ngx_http_request_t * r) {
-
 	flug_log_cstr("flug_request_handler");
 	ngx_int_t rc;
 	ngx_http_module_t * ctx;
@@ -284,7 +431,7 @@ static ngx_int_t flug_request_handler (ngx_http_request_t * r) {
 	
 	//r->main->count++;  //dafaq iz dat
 
-	flug_log_cstr ("request_body() handled with upstream_init");	
+	flug_log_cstr ("request_body() handled with upstream_init");
 	rc = ngx_http_read_client_request_body(r, ngx_http_upstream_init);
 	if (rc >= NGX_HTTP_SPECIAL_RESPONSE) {
 		flug_log_cstr("Special response");
@@ -292,7 +439,7 @@ static ngx_int_t flug_request_handler (ngx_http_request_t * r) {
 	}
 
 	flug_log_cstr("Returning NGX_DONE");
-	return NGX_DONE;
+	return NGX_OK;
 }
 
 
